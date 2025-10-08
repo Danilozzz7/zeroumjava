2f0852c2eedd10adcb260dc1f09e6ab345914cf6
GuilhermeMedeiros24 <rm93782@estudante.fieb.edu.br>
2025-10-08 07:42:32 -0300

    banco de dados somee

--- src/main/java/com/itb/zeroumtcc/zeroum/ZeroUmApplication.java
+++ src/main/java/com/itb/zeroumtcc/zeroum/ZeroUmApplication.java
@@ -11,10 +11,10 @@ public class ZeroUmApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ZeroUmApplication.class, args);
 
-		System.out.println("\n✅ ZeroUm Estágios iniciado com sucesso!");
-		System.out.println("🔗 API REST disponível em: http://localhost:8080/api/v1");
-		System.out.println("🗃️ Conectado ao banco de dados: bd_ZeroUm (SQL Server)");
-		System.out.println("📦 Todos os componentes carregados: entidades, repositórios, controllers");
-		System.out.println("👨‍🎓 Alunos, 👩‍💼 Empresas e 🧑‍💻 Vagas prontos para integração!\n");
+		System.out.println("\n ZeroUm Estágios iniciado com sucesso!");
+		System.out.println(" API REST disponível em: http://localhost:8080/api/v1");
+		System.out.println(" Conectado ao banco de dados: bd_ZeroUm (SQL Server)");
+		System.out.println(" Todos os componentes carregados: entidades, repositórios, controllers");
+		System.out.println(" Alunos,  Empresas e  Vagas prontos para integração!\n");
 	}
 }
